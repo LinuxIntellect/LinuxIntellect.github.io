13c744d95ad6c177e72817df5bd7150211033a37
Zobaer Ahmed <mystudymytech@gmail.com>
2020-05-07 17:34:08 +0600

    First commit

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Phone: $phone \n Message: $message";
-$recipient = "showeb.arif@gmail.com";
+$recipient = "mystudymytech@gmail.com";
 $subject = "Contact Form Guthub";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
